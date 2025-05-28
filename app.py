a15d59b481d2738fd9326fc88952319803339f8c
MigasGlovacki <migasglovacki@gmail.com>
2025-05-28 20:42:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -125,4 +125,4 @@ def registrar_item():
     return jsonify({"erro": "Item desconhecido"}), 400
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    app.run(host="0.0.0.0", port=10000)
